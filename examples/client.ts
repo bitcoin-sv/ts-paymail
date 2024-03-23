9d92c094811f95b350ce2c62ebb5d76ea6ee7a50
Brandon Bryant <brandon.bryant002@gmail.com>
2024-03-23 16:10:30 -0400

    remove examples, move to new project

--- examples/client.ts
+++
@@ -1,8 +0,0 @@
-import PaymailClient from '../src/paymailClient/paymailClient.js'
-
-const client = new PaymailClient();
-
-(async () => {
-  const publicProfile = await client.getPublicProfile('satoshi@localhost')
-  console.log(publicProfile)
-})()
