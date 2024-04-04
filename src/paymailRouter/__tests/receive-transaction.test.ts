9baf4a09c8bf0cc944f1a3db361e48c459a5e359
Deggen <d.kellenschwiler@bsvblockchain.org>
2024-04-04 09:36:32 -0500

    don’t skip this test.

--- src/paymailRouter/__tests/receive-transaction.test.ts
+++ src/paymailRouter/__tests/receive-transaction.test.ts
@@ -47,7 +47,7 @@ describe('#Paymail Server - P2P Receive Transaction', () => {
     expect(response.body.txid).toEqual('5878f6efcb1aa3be389510ae2ff10d0368976bf867e8442b751908f19024f8dd')
   })
 
-  it.skip('should reject with invalid signature', async () => {
+  it('should reject with invalid signature', async () => {
     paymailClient.verifyPublicKey = jest.fn().mockResolvedValue({ match: true })
     const privateKey = PrivateKey.fromRandom()
     const tx = Transaction.fromHex('01000000012adda020db81f2155ebba69e7c841275517ebf91674268c32ff2f5c7e2853b2c010000006b483045022100872051ef0b6c47714130c12a067db4f38b988bfc22fe270731c2146f5229386b02207abf68bbf092ec03e2c616defcc4c868ad1fc3cdbffb34bcedfab391a1274f3e412102affe8c91d0a61235a3d07b1903476a2e2f7a90451b2ed592fea9937696a07077ffffffff02ed1a0000000000001976a91491b3753cf827f139d2dc654ce36f05331138ddb588acc9670300000000001976a914da036233873cc6489ff65a0185e207d243b5154888ac00000000')
